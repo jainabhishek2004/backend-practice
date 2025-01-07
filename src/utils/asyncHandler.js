5ce495bbac04f8a327de18e6efbd7371672ac002
jainabhishek2004 <jainabhishekarham@gmail.com>
2025-01-07 13:08:48 +0530

    utils code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,18 +1,9 @@
-const ansyncHandler = (requestHandler) =>{
-(req,res,next)=> {
-    Promise.resolve(requestHandler(req,res,next)).
-    catch((err)=> next(err))
-}
-
-}
-
-
-
-
-
-
-
-
-
-export{ansyncHandler}
-
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err));  
+    };
+  };
+  
+  export { asyncHandler };
+  
